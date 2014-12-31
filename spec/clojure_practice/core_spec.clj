d77bd7e1366a4a4aa0b5e4bcee0ba1c581034204
Shivam Thapar <shivam.thapar1@gmail.com>
2014-12-30 23:51:56 -0800

    euler 2

--- spec/clojure_practice/core_spec.clj
+++ spec/clojure_practice/core_spec.clj
@@ -8,6 +8,10 @@
 
 (describe "Euler Prob 1"
   (it "tests if sum of all multiples of 3 or 5 below 1000 == 233168"
-    (should (= (mult-3-5) 233168))))
+    (should (= (mult-3-5 1000) 233168))))
+
+(describe "Euler Prob 2"
+  (it "tests if sum of even fibonacci numbers <=4000000 is 4613732"
+      (should (= (even-fib 4000000) 4613732))))
 
 (run-specs)
--- src/clojure_practice/core.clj
+++ src/clojure_practice/core.clj
@@ -4,6 +4,14 @@
   (= 5 n))
 
 (defn mult-3-5 "Find the sum of all the multiples of 3 or 5 below 1000." 
-  []
-  (reduce + (filter (fn [x] (or (zero? (mod x 3)) (zero? (mod x 5)))) (range 1000)))
+  [a]
+  (reduce + (filter (fn [x] (or (zero? (mod x 3)) (zero? (mod x 5)))) (range a)))
+)
+(defn even-fib "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."
+  [max]
+  (reduce + 
+          (filter even? 
+               (take-while (fn [x] (<= x max)) 
+                  (map first 
+                       (iterate (fn [[x y]] [y (+ x y)]) [0 1])))))
 )
